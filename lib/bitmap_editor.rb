eb3f137761fe59836e6e7406b42d664b37b43cac
cooknat <natalie.t.cook@gmail.com>
2018-02-02 13:12:00 +0000

    check input for validity

--- lib/bitmap_editor.rb
+++ lib/bitmap_editor.rb
@@ -5,29 +5,121 @@ class BitmapEditor
     return puts "please provide correct file" if file.nil? || !File.exists?(file)
 
     File.open(file).each do |line|
-      line = line.chomp
-      p line
-      # add verification for input validity/format
-      args = line.split(' ')   
-      case args[0]
-      when 'I'       
-        create(args[1].to_i,args[2].to_i)
-      when 'C'        
-      when 'L'  
-        colour(args[1].to_i, args[2].to_i, args[3])
-      when 'V'  
-      when 'H'
-      when 'S'
-          puts "There is no image"
+      line = line.chomp       
+      args = line.split(' ')
+      p args
+      if valid?(args)         
+        case args[0]
+        when 'I'       
+          create(args[1].to_i, args[2].to_i)
+        when 'C'        
+        when 'L'  
+          colour(args[1].to_i, args[2].to_i, args[3])
+        when 'V'  
+        when 'H'
+        when 'S'       
+            puts "There is no image"
+            break
+        else
+            puts 'unrecognised command :('
+            break
+        end
       else
-          puts 'unrecognised command :('
-      end
+        # need to change message based on reason for failure
+        puts "There is invalid input in the command:  #{line} - please fix and resubmit."  
+        break
+      end 
+       
     end
-  end
-  
+  end  
+
+  def valid?(args)
+    if @bitmap == nil && args[0] != 'I'
+      p "You need to create a bitmap before you can manipulate it. Please ensure the first line is an 'I' command."
+      return false
+    end  
+
+    #bitmapRows = @bitmap[0].size-1 ||= 0
+    #bitmapCols = @bitmap.size-1 ||= 0
+
+    if @bitmap != nil
+      bitmapRows = @bitmap[0].size-1 
+      bitmapCols = @bitmap.size-1 
+    else
+      bitmapRows = 0
+      bitmapCols = 0
+    end  
+
+    lookup = {
+      'I'=> {
+           :len => 3,
+           :rows => (1..250).to_a,
+           :columns => (1..250).to_a
+        }, 
+      'C'=> {
+           :len => 1
+        },
+      'L'=> {
+           :len => 4,
+           :column => (0..bitmapCols).to_a,
+           :row => (0..bitmapRows).to_a,           
+           :colour => ('A'..'Z').to_a
+        }, 
+      'H'=> {
+           :len => 5,           
+           :startCol => (0..bitmapCols).to_a,
+           :endCol => (0..bitmapCols).to_a,
+           :row => (0..bitmapRows).to_a,
+           :colour => ('A'..'Z').to_a
+        }, 
+      'V'=> {
+           :len => 5,
+           :column => (0..bitmapCols).to_a,
+           :startRow => (0..bitmapRows).to_a,
+           :endRow => (0..bitmapRows).to_a,
+           :colour => ('A'..'Z').to_a
+        },       
+      'S'=> {
+           :len => 1
+      }
+    }
+
+    settings = lookup[args[0]]   
+
+    case args[0]
+    when 'I'  
+      settings[:len] == args.length &&
+      settings[:rows].include?(args[1].to_i) &&
+      settings[:columns].include?(args[2].to_i)            
+    when 'L' 
+      settings[:len] == args.length &&
+      settings[:column].include?(args[1].to_i) &&
+      settings[:row].include?(args[2].to_i) &&    
+      settings[:colour].include?(args[3])             
+    when 'H' 
+      settings[:len] == args.length &&
+      settings[:startCol].include?(args[1].to_i) &&
+      settings[:endCol].include?(args[2].to_i) &&
+      settings[:row].include?(args[3].to_i) &&    
+      settings[:colour].include?(args[4])            
+    when 'V'
+      settings[:len] == args.length &&
+      settings[:column].include?(args[1].to_i) &&
+      settings[:startRow].include?(args[2].to_i) &&
+      settings[:endRow].include?(args[3].to_i) &&    
+      settings[:colour].include?(args[4])     
+    when 'S'
+      settings[:len] == args.length         
+    when 'C'
+      settings[:len] == args.length              
+    end            
+        
+  end  
+
+ 
   # represent bitmap with a two dimensional array
-  def create(m, n)
-   @bitmap = Array.new(n){Array.new(m, 'O')}     
+  def create(rows, cols)
+   @bitmap = Array.new(rows){Array.new(cols, 'O')}    
    p @bitmap
   end 
 
@@ -54,3 +146,4 @@ class BitmapEditor
   end 
 
 end
+
--- spec/bitmap_editor_spec.rb
+++ spec/bitmap_editor_spec.rb
@@ -6,10 +6,30 @@ bitmapEditor = BitmapEditor.new
 describe '#run' do
 end	
 
+describe '#valid?' do
+	it 'returns false if the argument array is the wrong size' do	
+	  expect(bitmapEditor.valid?(['V', 3, 4, 'G'])).to be_falsey
+	end	
+
+  it 'returns false if any of the arguments are not integers or single letters' do
+ 	  expect(bitmapEditor.valid?(['L', 'tiger', '*', 's'])).to be_falsey
+  end	   
+
+  it 'returns false if the specified pixels are not within the bounds of the bitmap' do
+		bitmapEditor.create(3,4)
+		expect(bitmapEditor.valid?(['H', 8, 8, 8, 'P'])).to be_falsey
+	end	
+
+	it 'returns true if the specified pixels are within the bounds of the bitmap' do
+		bitmapEditor.create(3,4)
+		expect(bitmapEditor.valid?(['H', 1, 2, 3, 'P'])).to be_truthy
+	end	
+end  
+
 describe '#create' do 
 	it 'creates a white bitmap of specified size' do	     
 		 bitmapEditor.create(3,4)
-	    expect(bitmapEditor.bitmap).to eq([['O','O','O'],['O','O','O'],['O','O','O'],['O','O','O']]) 
+	    expect(bitmapEditor.bitmap).to eq([['O','O','O','O'],['O','O','O','O'],['O','O','O','O']]) 
 	end
 end
 
